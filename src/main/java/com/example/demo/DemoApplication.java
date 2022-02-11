c5f6db61d1336f676d035e918e32eb5185737495
Prachi151 <99466088+Prachi151@users.noreply.github.com>
2022-02-11 16:22:38 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 // This is a main method
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		//brain commit ho g"ya h
+		System.out.println("kuch bhi matlab, prachi pagal h");
 	}
 
 }
